bc793bd8dade605cc46511e29c42a1906da40154
Aditya Balaji <143586625+Neo-04@users.noreply.github.com>
2025-10-08 21:36:58 +0530

    Update navigation.ts

--- config/navigation.ts
+++ config/navigation.ts
@@ -5,12 +5,12 @@ export interface NavigationItem {
 import { LayoutDashboard, FileText, Users, KanbanSquare, User, FolderKanban, BarChart4, Mail } from "lucide-react";
 export const primaryNavigation = [
   { name: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
+  { name: "KPI", href: "/kpi", icon: BarChart4 },
   { name: "DPR", href: "/dpr", icon: FileText },
   { name: "Employees", href: "/employees", icon: Users },
-  { name: "Profile", href: "/profile", icon: User },
   { name: "Projects", href: "/projects", icon: FolderKanban },
-  { name: "KPI", href: "/kpi", icon: BarChart4 },
   { name: "APAR", href: "/apar", icon: BarChart4 },
+  { name: "Profile", href: "/profile", icon: User },
   { name: "Settings", href: "/settings", icon: Mail },
 ];
 
